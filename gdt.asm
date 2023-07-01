b11462ff057d3225f16e05d5375d9b501b8fed33
Albert Ariel Widiaatmaja <albertarielwidiaatmaja@Alberts-MacBook-Pro.local>
2023-07-02 01:05:16 +0800

    feat: adds gdt descriptor and routine to enter protected mode

---
+++ gdt.asm
@@ -0,0 +1,14 @@
+gdt:
+    ; in x86, first entry is null descriptor
+    null_descriptor :   dw 0, 0, 0, 0
+    ; code and data segment of kernel
+    kernel_code_descriptor  : dw 0xffff, 0x0000, 0x9a00, 0x00cf
+    kernel_data_descriptor  : dw 0xffff, 0x0000, 0x9200, 0x00cf
+    ; code and data segment of user application
+    userspace_code_descriptor   : dw 0xffff, 0x0000, 0xfa00, 0x00cf
+    userspace_data_descriptor   : dw 0xffff, 0x0000, 0xf200, 0x00cf
+
+gdtr:
+    gdt_size_in_bytes   :   dw ( 5 * 8 )
+    gdt_base_address    :   dd gdt
+
