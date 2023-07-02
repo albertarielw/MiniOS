22b21d4e5e52d188201a9c2f24e9bb0f6b53cf46
Albert Ariel Widiaatmaja <albertarielwidiaatmaja@Alberts-MacBook-Pro.local>
2023-07-02 14:26:27 +0800

    feat: setup interrupt handler

--- main.c
+++ main.c
@@ -60,3 +60,9 @@ void printi(int number) {
     printi(number);
     printi(remaining);
 }
+
+void interrupt_handler(int interrupt_number) {
+    println();
+    print("Interrupt Received");
+    printi(interrupt_number);
+}
