c607fb44c3317b82ad241c687cea4891dd4ec777
Albert Ariel Widiaatmaja <albertarielwidiaatmaja@Alberts-MacBook-Pro.local>
2023-07-02 14:37:50 +0800

    fix: check main.c

--- main.c
+++ main.c
@@ -10,59 +10,70 @@ volatile unsigned char *video = 0xB8000;
 int nextTextPos = 0;
 int currLine = 0;
 
-void print(char *);
+void print( char * );
 void println();
-void printi(int);
+void printi( int );
 
-void kernel_main() {
+void kernel_main()
+{
     // video[0] = 'A';
     // video[2] = 'B' why not video[1]? this is because the byte after char contains color information
     // Each memory location corresponds to Cartesian coordinate [k * 2] = (k % max_x, k / max_x) e.g. [0] = (0, 0), [2] = (1, 0)
-    print("Welcome to MiniOS!");
-    println();
-    print("We are now in Protected-Mode");
-    println();
+	print( "Welcome to MiniOS!" );
+	println();
+	print( "We are now in Protected-mode" );
+	println();
+	
+	while( 1 );
+}
 
-    while(1);
+void interrupt_handler( int interrupt_number )
+{
+	println();
+	print( "Interrupt Received " );
+	printi( interrupt_number );
 }
 
 // iterates through character in a string until \0 is found
 // for each character, figures out the right position to print (based on memory location) and print it onto screen
-void print(char *str) {
-    int currCharLocationInVidMem, currColorLocationInVidMem;
-
-    while (*str != '\0') {
+void print( char *str )
+{
+	int currCharLocationInVidMem, currColorLocationInVidMem;
+	
+	while ( *str != '\0' )
+	{
         currCharLocationInVidMem = nextTextPos * 2;
-        currColorLocationInVidMem = currCharLocationInVidMem + 1;
-
-        video[currCharLocationInVidMem] = *str;
-        video[currColorLocationInVidMem] = 15;
-
-        nextTextPos++;
-        str++;
-    }
+		currColorLocationInVidMem = currCharLocationInVidMem + 1;
+		
+		video[ currCharLocationInVidMem ] = *str;
+		video[ currColorLocationInVidMem ] = 15;
+		
+		nextTextPos++;
+		
+		str++;
+	}
 }
 
-void println() {
-    nextTextPos = ++currLine * 80; // because the screen width in 03h is 80
+void println()
+{
+	nextTextPos = ++currLine * 80; // because the screen width in 03h is 80
 }
 
-void printi(int number) {
-    char* digitToStr[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-
-    if (number >= 0 && number <= 9) {
-        print(digitToStr[number]);
-        return;
-    }
-
-    int remaining = number % 10;
-    number = number / 10;
-    printi(number);
-    printi(remaining);
-}
-
-void interrupt_handler(int interrupt_number) {
-    println();
-    print("Interrupt Received");
-    printi(interrupt_number);
+void printi( int number )
+{
+	char* digitToStr[] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+	
+	if ( number >= 0 && number <= 9 )
+	{
+		print( digitToStr[ number ] );
+		return;
+	}
+	else
+	{
+		int remaining = number % 10;
+		number = number / 10;
+		
+		printi( number );
+		printi( remaining );
+	}
 }
