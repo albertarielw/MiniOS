b6e83ff882753e5454ac75bc2cc959456bb92248
Albert Ariel Widiaatmaja <albertarielwidiaatmaja@Alberts-MacBook-Pro.local>
2023-07-09 01:29:39 +0800

    fix: find out build issue, it is due to linker issue on mac, need to do linker on WSL

--- bootstrap.asm
+++ bootstrap.asm
@@ -110,7 +110,7 @@ printing_finished:
 
 	ret
 
-title_string        db  'The Bootloader of 539kernel.', 0
+title_string        db  'The Bootloader of MiniOS.', 0
 message_string      db  'The kernel is loading...', 0
 load_error_string   db  'The kernel cannot be loaded', 0
 number_of_sectors_to_load 	db 	10d ; 7.5KB will be loaded to memory, if kernel > 7.5KB we can modify this
